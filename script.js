07c4c2169e0c54665f96ab41c35b8f5c30bc3759
kairat <jumanov7070@gmail.com>
2022-05-19 20:26:13 +0600

    third commit

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
+const body = document.body;
+  const nav = document.querySelector('nav')
+navLink = document.querySelectorAll('.navlink');
+const hamburger = document.querySelector('hamburger');
+hamburger.addEventListener('click', toggleClass);
+   
 function toggleClass() {
     const menu = document.querySelector('.navbar');
     menu.classList.toggle('toggleClass');
+    body.classList.toggle('noscroll');
+
 }
 
 
-const hamburger = document.querySelector('hamburger');
-hamburger.addEventListener('click', toggleClass);
+toggleClass();
